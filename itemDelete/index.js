12cf48a028780f3afbf784c115b9e938637c7c43
Kristopher Bash <krisbash@hotmail.com>
2019-03-29 16:51:22 -0700

    initial commit

--- itemDelete/index.js
+++ itemDelete/index.js
@@ -1,27 +1,44 @@
 var request = require("request");
 var async = require('async');
+const reqAuthentication = true;
 
 module.exports = async function (context, req) {
     context.log('Item delete received');
-    context.log(1)
-    var isAuthenticated = await authenticate()
+
+    if (reqAuthentication ){
+        var clientCert = "foo"
+        var isAuthenticated = await authenticate(clientCert);
+        context.log(isAuthenticated);
+    }
+    context.log("delete item")
+    var result = await deleteItem(req);
+
     context.res = {
-        status: 200,
-        body: "ffoo"
+        status: result
     };
+    
 };
 
-async function authenticate(){
-    return new Promise((resolve, reject) => {
-        var metadataUri = "https://mgmtexp-svc-int.trafficmanager.net/metadata/authentication"
-        if(1 < 2 ){
-            resolve(2)
-        }
-       else{
-            reject(false)
-        }
+async function deleteItem(req){
+    return new Promise((resolve,reject) => {
+        var subscriptionId = req.params.subscriptionId
+        var rgName = req.params.rgame        
+        var resourceName = req.params.resourceName
+        //delete item, return 200 on success
+        resolve(200)
+        //if item already delete, return 204
     });
-}
+};
+
+async function authenticate(clientCert){
+    return new Promise((resolve, reject) => {
+        var metadataUri = "https://www.google.com"
+        request(metadataUri, { json: true }, (err, res, body) => {
+            if (err) { reject(err) };
+            resolve(body);
+        });
+    });
+};
 
  
   
